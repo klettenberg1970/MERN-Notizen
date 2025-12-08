576b85f19f976b188689966cef9264bb957c7482
klettenberg1970 <bastenstefan@gmailcom>
2025-12-08 10:59:40 +0100

    1. Update 8.12.

--- Client/src/pages/Logging/logging.jsx
+++ Client/src/pages/Logging/logging.jsx
@@ -1,16 +1,14 @@
 
-
-import './logging.css';
-
-
+import '../../assets/login.css';
 import LoggingFormular from "./loggingformular"
+import Logginstart from './loggingstart';
 
 export default function Logging() {
 
     return (
 
         <div>
-
+<Logginstart/>
 
             <LoggingFormular />
 
---
+++ Client/src/pages/Logging/loggingstart.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Logginstart (){
+
+    return(
+        <div>
+            <img src="phoenix.jpg" alt="Phoenix" />
+        </div>
+    )
+}
